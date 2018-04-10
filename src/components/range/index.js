511afaab1a9de1131a981a5074285394f0816ecf
Ambalika <shouvikbhuiyan1990@gmail.com>
2018-04-10 08:07:13 +0530

    styling

--- src/components/range/index.js
+++ src/components/range/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './index.css';
 
-export default ({ value,footnote,onRangeChange })=> (
-    <div>
-        <input 
-            type="range" 
-            orient="vertical" 
-            min="-12" max="12" 
-            value={value}
-            onChange={onRangeChange}
-        />
-        <p>{footnote}</p>
-    </div>
-);
+export default ({ value,footnote,onRangeChange })=> {
+    let className = '';
+    if( value>0 ){
+        className = 'red';
+    }else if(value<0){
+        className = 'green';
+    }
+    return(
+        <div>
+            <input 
+                type="range" 
+                orient="vertical" 
+                min="-12" max="12" 
+                value={value}
+                className={className}
+                onChange={onRangeChange}
+            />
+            <p>{footnote}</p>
+        </div>
+    )
+};
